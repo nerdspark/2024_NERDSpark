84ed809b3e3d4e88b1fb38b01ba15d518b92ee3e
Amogh90 <hgomam@gmail.com>
2024-02-03 19:40:28 -0500

    Fixed Camera height

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,14 +26,14 @@ public final class Constants {
          * Physical location of the left camera on the robot, relative to the center of the robot.
          */
         public static final Transform3d ROBOT_TO_FRONT_CAMERA = new Transform3d(
-                new Translation3d(Units.inchesToMeters(15.5), Units.inchesToMeters(0), Units.inchesToMeters(2.5)),
+                new Translation3d(Units.inchesToMeters(15.5), Units.inchesToMeters(0), Units.inchesToMeters(6.5)),
                 new Rotation3d(0, Math.toRadians(30), Math.toRadians(0)));
 
         /**
          * Physical location of the back camera on the robot, relative to the center of the robot.
          */
         public static final Transform3d ROBOT_TO_BACK_CAMERA = new Transform3d(
-                new Translation3d(-Units.inchesToMeters(15.5), Units.inchesToMeters(0), Units.inchesToMeters(2.5)),
+                new Translation3d(-Units.inchesToMeters(15.5), Units.inchesToMeters(0), Units.inchesToMeters(6.5)),
                 new Rotation3d(0, Math.toRadians(25), Math.toRadians(180)));
 
         /** Minimum target ambiguity. Targets with higher ambiguity will be discarded */
