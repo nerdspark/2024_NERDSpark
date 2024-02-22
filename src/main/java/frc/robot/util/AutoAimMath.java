a29afbf09bd39884f528a8b2e60ebe0313bddb68
Amogh90 <hgomam@gmail.com>
2024-02-21 19:05:45 -0500

    Repushing with lowercase folders

---
+++ src/main/java/frc/robot/util/AutoAimMath.java
@@ -0,0 +1,33 @@
+package frc.robot.util;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.Constants.speakerConstants;
+import java.util.function.Supplier;
+
+public class AutoAimMath {
+    Supplier<Pose2d> poseSupplier;
+    Pose2d targetPose;
+
+    public static Rotation2d getAutoAimCalcRobot(Supplier<Pose2d> poseSupplier, Translation2d targetPose) {
+
+        double robotAimAngle = Math.atan2(
+                targetPose.getY() - poseSupplier.get().getY(),
+                AllianceFlipUtil.apply(targetPose.getX()) - poseSupplier.get().getX()); // Robot Angle
+
+        return new Rotation2d(robotAimAngle);
+    }
+
+    public static double getAutoAimCalcFourBar(Supplier<Pose2d> poseSupplier, Pose2d targetPose) {
+
+        double fourBarAngle = Math.atan2(
+                speakerConstants.speakerHeight,
+                poseSupplier
+                        .get()
+                        .getTranslation()
+                        .getDistance(AllianceFlipUtil.apply(targetPose.getTranslation()))); // Shooter Angle
+
+        return fourBarAngle;
+    }
+}
